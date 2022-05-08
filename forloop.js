ace82ab7f888ccdb7cfe2524410472523f47323f
DeveloperMahmud <mrepte700@gmail.com>
2022-05-09 01:24:43 +0600

    function & forloop array tracking codes

--- forloop.js
+++ forloop.js
@@ -13,3 +13,13 @@ for (var i = 0; i < nums.length; i++) {
     var elements = nums[i];
     console.log(elements)
 };
+
+console.log('-----------');
+
+// get array using for loop 
+var friends = ["diago","rin","nobody","prince","mahmud"];
+
+for (var j = 0; j < friends.length; j++) {
+    var elements = friends[j];
+    console.log(elements)
+};
