5e84b715259649b919fa4b55a15ca1014c3958c5
DeveloperMahmud <mrepte700@gmail.com>
2022-05-10 21:45:26 +0600

    function codes updated

--- function.js
+++ function.js
@@ -3,5 +3,20 @@ function missYou(){
     console.log("Want to see you dear mgh");
 }
 
-console.log("hello,");
-missYou();
+// console.log("hello,");
+// missYou();
+
+function doubleNum(num){
+    var result = num*2;
+    return result;
+}
+var output = doubleNum(80);
+console.log(output);
+
+function sub (num1,num2) {
+    var subResult = (num1-num2);
+    return subResult;
+}
+
+var subOutput = sub(30, 15);
+console.log(subOutput);   
