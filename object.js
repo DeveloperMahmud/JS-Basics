8c8b07f297256efb4e6db8559e4c6d824d29366a
DeveloperMahmud <mrepte700@gmail.com>
2022-05-10 22:13:02 +0600

    object.js file and some codes added

---
+++ object.js
@@ -0,0 +1,8 @@
+var profile1 = { Name:"Prince", Age:21, Followers:4377, RelationshipStatus: "Single"};
+var profile2 = { Name:"John", Age:25, Followers:432, RelationshipStatus: "Married"};
+var profile3 = { Name:"Kelly", Age:29, Followers:2330, RelationshipStatus: "Secret"};
+
+console.log(profile1);
+console.log(profile1.Name);
+console.log(profile2);
+console.log(profile3);
