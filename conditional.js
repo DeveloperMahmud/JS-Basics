f802cc385e9232d88c530aece876c5e646d5b02f
DeveloperMahmud <mrepte700@gmail.com>
2022-05-12 01:51:13 +0600

    conditional js file added for if else conditional code basic practicing

---
+++ conditional.js
@@ -0,0 +1,12 @@
+var jobPaiso = false;
+var savingAmmount = 300000;
+
+if(jobPaiso ==true && savingAmmount > 200000){
+    console.log("okay done! esho tomar patri khuji");
+}
+else if( jobPaiso == true){
+    console.log("ok, kichudin por dekhbo");
+}
+else{
+    console.log("tor kopale biya nai...");
+};
