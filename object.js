a6c980c21ee971e4a0d7de8f79a0a33e0356e39f
DeveloperMahmud <mrepte700@gmail.com>
2022-05-12 01:39:52 +0600

    adding propertise in objects

--- object.js
+++ object.js
@@ -1,9 +1,15 @@
-var profile1 = { Name:"Prince", Age:21, Followers:4377, RelationshipStatus: "Single"};
+var profile1 = { Name:"Prince", Age:21, Followers:4377, RelationshipStatus: "Married"};
 var profile2 = { Name:"John", Age:25, Followers:432, RelationshipStatus: "Married"};
 var profile3 = { Name:"Kelly", Age:29, Followers:2330, RelationshipStatus: "Secret"};
 
+// console.log(profile1);
+// console.log(profile1.Name);
+// console.log(profile2.Followers);
+// console.log(profile2.Age);
+// console.log(profile3);
+
+//input propertise
+profile1.Hobby = "Travelling, Gardening, Gaming, Freelancing";
+profile1.LifePartner = "mgh cndl";
+profile1.NickName = "Pusku";
 console.log(profile1);
-console.log(profile1.Name);
-console.log(profile2.Followers);
-console.log(profile2.Age);
-console.log(profile3);
